ab05612384d0ec1bdec15dd377dee11a9197071c
remintroy <reminremin01@gmail.com>
2023-02-15 12:38:51 +0530

    Question Server

--- question/src/index.ts
+++ question/src/index.ts
@@ -1,1 +1,16 @@
-console.log('question server');
+import Express from "express";
+
+const appConfig = {
+  name: "Question server",
+  port: process.env.PORT || 4003,
+  baseUrl: "/",
+};
+const server = Express();
+const app = Express.Router();
+
+app.get("/test", (req, res) => {
+  res.send(`<h1>${appConfig.name} is running</h1>`);
+});
+
+server.use(appConfig.baseUrl, app);
+server.listen(appConfig.port, () => console.log(`[-] Question server started on port ${appConfig.port}`));
